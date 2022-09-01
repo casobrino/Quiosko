54c2339fa5415e32a42c89cfca906a903f9d275b
Cesar Sobrino <casobrino_0@hotmail.com>
2022-08-31 23:33:02 -0500

    Pedidos enviados al servidor mediante Prisma y mejora en la experiencia de usuario

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, createContext } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { useRouter } from 'next/router'
 
 const QuioscoContext = createContext();
 
@@ -11,6 +12,9 @@ const QuioscoProvider = ({ children }) => {
     const [producto, setProducto] = useState({})
     const [modal, setModal] = useState(false)
     const [pedido, setPedido] = useState([])
+    const [nombre, setNombre] = useState('')
+    const [total, setTotal] = useState(0.0)
+    const router = useRouter()
 
     //Hace una consulta axios a la api
     const obtenerCategorias = async () => {
@@ -28,10 +32,20 @@ const QuioscoProvider = ({ children }) => {
         setCategoriaActual(categorias[0]) //asigna la llave 0
     }, [categorias])
 
+
+    //Effect que cambia con cada producto y actualiza el total
+    useEffect(() => {
+        const nuevoTotal = pedido.reduce((total, producto) => (producto.precio * producto.cantidad) + total, 0)
+
+        setTotal(nuevoTotal)
+    }, [pedido])
+
+
     //Metodo para obtener la categoria seleecionada
     const handleClicCategoria = id => {
         const categoria = categorias.filter(cate => cate.id === id)
         setCategoriaActual(categoria[0]);
+        router.push('/')
     }
 
     //Agregar producto para seleccionado
@@ -39,6 +53,7 @@ const QuioscoProvider = ({ children }) => {
         setProducto(producto)
     }
 
+    //Cambia el modal, la alerta a otro estado inmediato
     const handleChangeModal = () => {
         setModal(!modal)
     }
@@ -63,6 +78,7 @@ const QuioscoProvider = ({ children }) => {
 
     }
 
+    //Edita cantidad filtrandolo mientras el id sea el mismo
     const handleEditarCantidades = (id) => {
         const productoActualizar = pedido.filter(producto => producto.id === id)
         setProducto(productoActualizar[0])
@@ -70,10 +86,33 @@ const QuioscoProvider = ({ children }) => {
         setModal(!modal)
     }
 
+    //Filtra todos los pedidos menos el dado - ELIMINA
     const handleEliminarProducto = (id) => {
         const pedidoActualizado = pedido.filter(producto => producto.id !== id)
         setPedido(pedidoActualizado)
 
+    }
+
+    //Coloca orden con el pedido y el total
+    const colocarOrden = async (e) => {
+        e.preventDefault()
+        try {
+            await axios.post('/api/ordenes', { pedido, nombre, total, fecha: Date.now().toString() })
+
+            //Resetear la app
+            setCategoriaActual(categorias[0])
+            setPedido([])
+            setNombre('')
+            setTotal(0.0)
+
+            toast.success('Pedido realizado correctamente')
+            setTimeout(() => {
+                router.push('/')
+            }, 2000);
+
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return (
@@ -85,12 +124,16 @@ const QuioscoProvider = ({ children }) => {
                 categoriaActual,
                 modal,
                 pedido,
+                nombre,
+                total,
                 handleClicCategoria,
                 handleSetProducto,
                 handleChangeModal,
                 handleAgregarPedido,
                 handleEditarCantidades,
-                handleEliminarProducto
+                handleEliminarProducto,
+                setNombre,
+                colocarOrden
             }}
         >
             {children}
--- pages/total.jsx
+++ pages/total.jsx
@@ -1,9 +1,59 @@
+import { useEffect, useCallback } from "react";
 import Layout from "../layout/Layout"
-export default function Total(){
-    return(
-        <Layout pagina = 'Total y confirmar pedido'>
+import useQuiosco from "../Hooks/UseQuiosco";
+import { formatearDinero } from '../helpers/index'
+
+export default function Total() {
+    const { pedido, nombre, setNombre, colocarOrden, total } = useQuiosco();
+
+    const comprobarPedido = useCallback(() => {
+        return pedido.length === 0 || nombre === ''
+    }, [pedido, nombre]);
+
+    useEffect(() => {
+        comprobarPedido();
+    }, [pedido], comprobarPedido)
+
+    return (
+        <Layout pagina='Total y confirmar pedido'>
             <h1 className="text-4xl font-black">Resumen</h1>
             <p className="text-3xl my-10">Confirma tu pedido a continuacion</p>
-        </Layout>
+
+            <form
+                onSubmit={colocarOrden}
+            >
+                <div className="">
+                    <label
+                        htmlFor="nombre"
+                        id="nombre"
+                        className="block uppercase text-slate-800 font-bold text-xl"
+                    >
+                        Nombre
+                    </label>
+                    <input
+                        type="text"
+                        className="bg-gray-200 w-full lg:w-1/3 mt-3 p-2 rounded-md"
+                        value={nombre}
+                        onChange={(e) => setNombre(e.target.value)} />
+                    <div className="mt-10">
+                        <p className="text-2xl">
+                            Texto total a pagar: {' '} <span className="font-bold">{formatearDinero(total)}</span>
+
+                        </p>
+                    </div>
+                </div>
+                <div className="mt-5">
+                    <input
+                        type='submit'
+                        className={`${comprobarPedido()
+                            ? 'bg-indigo-100'
+                            : 'bg-indigo-600 hover:bg-indigo-800'
+                            }  w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-white text-center`}
+                        value="Confirmar Pedido"
+                        disabled={comprobarPedido()}
+                    />
+                </div>
+            </form>
+        </Layout >
     )
 }
